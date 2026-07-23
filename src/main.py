b48c90f3133045fc73d2f59fc5c3f260d2bb0d2c
BlinovLS <blinovleonid9@gmail.com>
2026-07-24 02:52:08 +0300

    First version complete.

---
+++ src/main.py
@@ -0,0 +1,29 @@
+import os.path
+
+from kivy.app import App
+from kivy.lang.builder import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager
+
+from src.ui.mainscreen import MainScreen
+from src.file_manager.jsonmanager import JSONManager
+
+class WaterCupObserverApp(App):
+
+    json_file_manager:JSONManager
+
+    def build(self):
+
+        self.json_file_manager = JSONManager(self)
+
+        main_screen_kv = os.path.join(os.path.dirname(__file__), 'ui', 'kv', 'main_screen.kv')
+        Builder.load_file(main_screen_kv)
+
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(MainScreen(name='main'))
+
+        return screen_manager
+
+
+if __name__ == '__main__':
+    WaterCupObserverApp().run()
